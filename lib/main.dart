b59eb75c4dfdd658c69b1dd60660c96bab56e4ca
Ramon Abreu <53264058+ramonabreu-cdev@users.noreply.github.com>
2020-06-02 21:52:49 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Lista de compras"),
+        title: Text("Lista de Compras"),
         backgroundColor: Colors.red,
         centerTitle: true,
       ),
